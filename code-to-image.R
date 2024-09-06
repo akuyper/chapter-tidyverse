ce6d3a03953bcbb52dd39eda940775cc35a94761
akuyper <a-kuyper@northwestern.edu>
2024-09-06 05:31:05 -0500

    conclusion

--- code-to-image.R
+++ code-to-image.R
@@ -1,14 +1,39 @@
-```{r}
-#| label: piping
-#| code-line-numbers: true
-#| eval: false
+library(tidyverse)
+library(patchwork)
 
-# non-piping
-f(g(h(my_data)))
+# ```{r}
+# #| label: piping
+# #| code-line-numbers: true
+# #| eval: false
+# 
+# # non-piping
+# f(g(h(my_data)))
+# 
+# # piping
+# my_data |> 
+#   h() |> 
+#   g() |> 
+#   f()
+# ```
 
-# piping
-my_data |> 
-  h() |> 
-  g() |> 
-  f()
-```
+load("data/county_cwift_by_income.rda")
+load("data/state_cwift_by_income.rda")
+
+
+# CWIFT heat maps
+(cwift_by_income_maps <-
+(state_cwift_by_income + county_cwift_by_income) +
+   plot_annotation(tag_levels = "A", tag_prefix = "(", tag_suffix = ")") +
+   plot_layout(guides = "collect") &
+   theme(
+     legend.position = "bottom",
+     plot.tag = element_text(size = 18)
+   ))
+
+ggsave(
+  filename = "plots/cwift_by_income_maps.png",
+  plot = cwift_by_income_maps,
+  height= 3.5,
+  width = 11,
+  units = "in"
+)
